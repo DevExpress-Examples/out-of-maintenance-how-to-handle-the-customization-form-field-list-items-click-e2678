71a6ec7d84fbc9b0f40156ea2d450ac00a148ad6
DevExpressExampleBot <fakeEmail>
2022-05-18 15:37:53 +0400

    Source auto update [skip ci]

--- VB/WindowsApplication53/Form1.Designer.vb
+++ VB/WindowsApplication53/Form1.Designer.vb
@@ -1,36 +1,36 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace WindowsApplication53
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-            Dim DataSourceColumnBinding1 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
-            Dim DataSourceColumnBinding2 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
-            Dim DataSourceColumnBinding3 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
-            Dim DataSourceColumnBinding4 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
-            Dim DataSourceColumnBinding5 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
-            Dim DataSourceColumnBinding6 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
+
+            MyBase.Dispose(disposing)
+        End Sub
+
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Dim dataSourceColumnBinding1 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Dim dataSourceColumnBinding2 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Dim dataSourceColumnBinding3 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Dim dataSourceColumnBinding4 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Dim dataSourceColumnBinding5 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Dim dataSourceColumnBinding6 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
             Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
             Me.dataSet1 = New System.Data.DataSet()
             Me.dataTable1 = New System.Data.DataTable()
@@ -44,14 +44,14 @@ Namespace WindowsApplication53
             Me.fieldValue1 = New DevExpress.XtraPivotGrid.PivotGridField()
             Me.fieldDate = New DevExpress.XtraPivotGrid.PivotGridField()
             Me.panel1 = New System.Windows.Forms.Panel()
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.dataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.dataTable1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dataSet1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dataTable1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
-            '
-            'pivotGridControl1
-            '
-            Me.pivotGridControl1.Cursor = System.Windows.Forms.Cursors.Default
+            ' 
+            ' pivotGridControl1
+            ' 
+            Me.pivotGridControl1.Cursor = System.Windows.Forms.Cursors.[Default]
             Me.pivotGridControl1.DataMember = "Data"
             Me.pivotGridControl1.DataSource = Me.dataSet1
             Me.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
@@ -62,129 +62,142 @@ Namespace WindowsApplication53
             Me.pivotGridControl1.OptionsData.DataProcessingEngine = DevExpress.XtraPivotGrid.PivotDataProcessingEngine.Optimized
             Me.pivotGridControl1.Size = New System.Drawing.Size(439, 410)
             Me.pivotGridControl1.TabIndex = 0
-            '
-            'dataSet1
-            '
+            AddHandler Me.pivotGridControl1.ShowingCustomizationForm, New DevExpress.XtraPivotGrid.CustomizationFormShowingEventHandler(AddressOf Me.pivotGridControl1_ShowingCustomizationForm)
+            AddHandler Me.pivotGridControl1.ShowCustomizationForm, New System.EventHandler(AddressOf Me.pivotGridControl1_ShowCustomizationForm)
+            AddHandler Me.pivotGridControl1.HideCustomizationForm, New System.EventHandler(AddressOf Me.pivotGridControl1_HideCustomizationForm)
+            ' 
+            ' dataSet1
+            ' 
             Me.dataSet1.DataSetName = "NewDataSet"
             Me.dataSet1.Tables.AddRange(New System.Data.DataTable() {Me.dataTable1})
-            '
-            'dataTable1
-            '
+            ' 
+            ' dataTable1
+            ' 
             Me.dataTable1.Columns.AddRange(New System.Data.DataColumn() {Me.dataColumn1, Me.dataColumn2, Me.dataColumn3})
             Me.dataTable1.TableName = "Data"
-            '
-            'dataColumn1
-            '
+            ' 
+            ' dataColumn1
+            ' 
             Me.dataColumn1.ColumnName = "Name"
-            '
-            'dataColumn2
-            '
+            ' 
+            ' dataColumn2
+            ' 
             Me.dataColumn2.ColumnName = "Date"
-            Me.dataColumn2.DataType = GetType(Date)
-            '
-            'dataColumn3
-            '
+            Me.dataColumn2.DataType = GetType(System.DateTime)
+            ' 
+            ' dataColumn3
+            ' 
             Me.dataColumn3.ColumnName = "Value"
             Me.dataColumn3.DataType = GetType(Decimal)
-            '
-            'fieldName1
-            '
+            ' 
+            ' fieldName1
+            ' 
             Me.fieldName1.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
             Me.fieldName1.AreaIndex = 0
-            DataSourceColumnBinding1.ColumnName = "Name"
-            Me.fieldName1.DataBinding = DataSourceColumnBinding1
+            dataSourceColumnBinding1.ColumnName = "Name"
+            Me.fieldName1.DataBinding = dataSourceColumnBinding1
             Me.fieldName1.Name = "fieldName1"
-            '
-            'fieldDate2
-            '
+            ' 
+            ' fieldDate2
+            ' 
             Me.fieldDate2.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
             Me.fieldDate2.AreaIndex = 0
             Me.fieldDate2.Caption = "Year"
-            DataSourceColumnBinding2.ColumnName = "Date"
-            DataSourceColumnBinding2.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.DateYear
-            Me.fieldDate2.DataBinding = DataSourceColumnBinding2
+            dataSourceColumnBinding2.ColumnName = "Date"
+            dataSourceColumnBinding2.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.DateYear
+            Me.fieldDate2.DataBinding = dataSourceColumnBinding2
             Me.fieldDate2.Name = "fieldDate2"
             Me.fieldDate2.Visible = False
-            '
-            'fieldValue3
-            '
+            ' 
+            ' fieldValue3
+            ' 
             Me.fieldValue3.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
             Me.fieldValue3.AreaIndex = 0
             Me.fieldValue3.Caption = "Sum"
-            DataSourceColumnBinding3.ColumnName = "Value"
-            Me.fieldValue3.DataBinding = DataSourceColumnBinding3
+            dataSourceColumnBinding3.ColumnName = "Value"
+            Me.fieldValue3.DataBinding = dataSourceColumnBinding3
             Me.fieldValue3.Name = "fieldValue3"
-            '
-            'fieldValue
-            '
+            ' 
+            ' fieldValue
+            ' 
             Me.fieldValue.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
             Me.fieldValue.AreaIndex = 1
             Me.fieldValue.Caption = "Count"
-            DataSourceColumnBinding4.ColumnName = "Value"
-            Me.fieldValue.DataBinding = DataSourceColumnBinding4
+            dataSourceColumnBinding4.ColumnName = "Value"
+            Me.fieldValue.DataBinding = dataSourceColumnBinding4
             Me.fieldValue.Name = "fieldValue"
             Me.fieldValue.Visible = False
-            '
-            'fieldValue1
-            '
+            ' 
+            ' fieldValue1
+            ' 
             Me.fieldValue1.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
             Me.fieldValue1.AreaIndex = 1
             Me.fieldValue1.Caption = "Average"
-            DataSourceColumnBinding5.ColumnName = "Value"
-            Me.fieldValue1.DataBinding = DataSourceColumnBinding5
+            dataSourceColumnBinding5.ColumnName = "Value"
+            Me.fieldValue1.DataBinding = dataSourceColumnBinding5
             Me.fieldValue1.Name = "fieldValue1"
             Me.fieldValue1.Visible = False
-            '
-            'fieldDate
-            '
+            ' 
+            ' fieldDate
+            ' 
             Me.fieldDate.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
             Me.fieldDate.AreaIndex = 0
             Me.fieldDate.Caption = "Date"
-            DataSourceColumnBinding6.ColumnName = "Date"
-            DataSourceColumnBinding6.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.[Date]
-            Me.fieldDate.DataBinding = DataSourceColumnBinding6
+            dataSourceColumnBinding6.ColumnName = "Date"
+            dataSourceColumnBinding6.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.[Date]
+            Me.fieldDate.DataBinding = dataSourceColumnBinding6
             Me.fieldDate.Name = "fieldDate"
             Me.fieldDate.Visible = False
-            '
-            'panel1
-            '
+            ' 
+            ' panel1
+            ' 
             Me.panel1.Dock = System.Windows.Forms.DockStyle.Right
             Me.panel1.Location = New System.Drawing.Point(439, 0)
             Me.panel1.Name = "panel1"
             Me.panel1.Size = New System.Drawing.Size(265, 410)
             Me.panel1.TabIndex = 1
-            '
-            'Form1
-            '
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
             Me.ClientSize = New System.Drawing.Size(704, 410)
             Me.Controls.Add(Me.pivotGridControl1)
             Me.Controls.Add(Me.panel1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.dataSet1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.dataTable1, System.ComponentModel.ISupportInitialize).EndInit()
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.dataSet1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.dataTable1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
 #End Region
+        Private pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
 
-        Private WithEvents pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
-		Private dataSet1 As System.Data.DataSet
-		Private dataTable1 As System.Data.DataTable
-		Private dataColumn1 As System.Data.DataColumn
-		Private dataColumn2 As System.Data.DataColumn
-		Private dataColumn3 As System.Data.DataColumn
-		Private fieldName1 As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldDate2 As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldValue3 As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldValue As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldValue1 As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldDate As DevExpress.XtraPivotGrid.PivotGridField
-		Private panel1 As System.Windows.Forms.Panel
-	End Class
+        Private dataSet1 As System.Data.DataSet
+
+        Private dataTable1 As System.Data.DataTable
+
+        Private dataColumn1 As System.Data.DataColumn
+
+        Private dataColumn2 As System.Data.DataColumn
+
+        Private dataColumn3 As System.Data.DataColumn
+
+        Private fieldName1 As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldDate2 As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldValue3 As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldValue As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldValue1 As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldDate As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private panel1 As System.Windows.Forms.Panel
+    End Class
 End Namespace
-
--- VB/WindowsApplication53/Form1.vb
+++ VB/WindowsApplication53/Form1.vb
@@ -1,97 +1,93 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraEditors
 Imports DevExpress.XtraPivotGrid.Customization
 
 Namespace WindowsApplication53
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			PopulateTable()
-			pivotGridControl1.RefreshData()
-			pivotGridControl1.BestFit()
-			pivotGridControl1.FieldsCustomization(panel1)
-		End Sub
-		Private Sub PopulateTable()
-			Dim myTable As DataTable = dataSet1.Tables("Data")
-			myTable.Rows.Add(New Object() {"Aaa", DateTime.Today, 7})
-			myTable.Rows.Add(New Object() { "Aaa", DateTime.Today.AddDays(1), 4 })
-			myTable.Rows.Add(New Object() { "Bbb", DateTime.Today, 12 })
-			myTable.Rows.Add(New Object() { "Bbb", DateTime.Today.AddDays(1), 14 })
-			myTable.Rows.Add(New Object() { "Ccc", DateTime.Today, 11 })
-			myTable.Rows.Add(New Object() { "Ccc", DateTime.Today.AddDays(1), 10 })
 
-			myTable.Rows.Add(New Object() { "Aaa", DateTime.Today.AddYears(1), 4 })
-			myTable.Rows.Add(New Object() { "Aaa", DateTime.Today.AddYears(1).AddDays(1), 2 })
-			myTable.Rows.Add(New Object() { "Bbb", DateTime.Today.AddYears(1), 3 })
-			myTable.Rows.Add(New Object() { "Bbb", DateTime.Today.AddDays(1).AddYears(1), 1 })
-			myTable.Rows.Add(New Object() { "Ccc", DateTime.Today.AddYears(1), 8 })
-			myTable.Rows.Add(New Object() { "Ccc", DateTime.Today.AddDays(1).AddYears(1), 22 })
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private _customizationForm As Form
-		Private Sub pivotGridControl1_ShowingCustomizationForm(ByVal sender As Object, ByVal e As DevExpress.XtraPivotGrid.CustomizationFormShowingEventArgs) Handles pivotGridControl1.ShowingCustomizationForm
-			_customizationForm = e.CustomizationForm
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub pivotGridControl1_ShowCustomizationForm(ByVal sender As Object, ByVal e As EventArgs) Handles pivotGridControl1.ShowCustomizationForm
-			If _customizationForm IsNot Nothing Then
-				Dim listControls As New List(Of ListBoxControl)()
-				FindListBox(_customizationForm, listControls)
-				For Each listBox As ListBoxControl In listControls
-					AddHandler listBox.MouseClick, AddressOf listBox_MouseClick
-				Next listBox
-				listControls.Clear()
-			End If
-		End Sub
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            PopulateTable()
+            pivotGridControl1.RefreshData()
+            pivotGridControl1.BestFit()
+            pivotGridControl1.FieldsCustomization(panel1)
+        End Sub
 
-		Private Sub pivotGridControl1_HideCustomizationForm(ByVal sender As Object, ByVal e As EventArgs) Handles pivotGridControl1.HideCustomizationForm
-			If _customizationForm IsNot Nothing Then
-				Dim listControls As New List(Of ListBoxControl)()
-				FindListBox(_customizationForm, listControls)
-				For Each listBox As ListBoxControl In listControls
-					RemoveHandler listBox.MouseClick, AddressOf listBox_MouseClick
-				Next listBox
-				listControls.Clear()
-				_customizationForm = Nothing
-			End If
-		End Sub
+        Private Sub PopulateTable()
+            Dim myTable As DataTable = dataSet1.Tables("Data")
+            myTable.Rows.Add(New Object() {"Aaa", Date.Today, 7})
+            myTable.Rows.Add(New Object() {"Aaa", Date.Today.AddDays(1), 4})
+            myTable.Rows.Add(New Object() {"Bbb", Date.Today, 12})
+            myTable.Rows.Add(New Object() {"Bbb", Date.Today.AddDays(1), 14})
+            myTable.Rows.Add(New Object() {"Ccc", Date.Today, 11})
+            myTable.Rows.Add(New Object() {"Ccc", Date.Today.AddDays(1), 10})
+            myTable.Rows.Add(New Object() {"Aaa", Date.Today.AddYears(1), 4})
+            myTable.Rows.Add(New Object() {"Aaa", Date.Today.AddYears(1).AddDays(1), 2})
+            myTable.Rows.Add(New Object() {"Bbb", Date.Today.AddYears(1), 3})
+            myTable.Rows.Add(New Object() {"Bbb", Date.Today.AddDays(1).AddYears(1), 1})
+            myTable.Rows.Add(New Object() {"Ccc", Date.Today.AddYears(1), 8})
+            myTable.Rows.Add(New Object() {"Ccc", Date.Today.AddDays(1).AddYears(1), 22})
+        End Sub
 
-		Private Sub listBox_MouseClick(ByVal sender As Object, ByVal e As MouseEventArgs)
-			Dim listBox As ListBoxControl = TryCast(sender, ListBoxControl)
-			Dim treeNode As PivotCustomizationTreeNodeBase = TryCast(listBox.SelectedItem, PivotCustomizationTreeNodeBase)
-			If treeNode IsNot Nothing Then
-				Me.Text = treeNode.Field.ToString()
-			End If
-		End Sub
+        Private _customizationForm As Form
 
-		Private Sub FindListBox(ByVal parentControl As Control, ByVal listControls As List(Of ListBoxControl))
-			For Each c As Control In parentControl.Controls
-				Dim listBox As ListBoxControl = TryCast(c, ListBoxControl)
-				If listBox IsNot Nothing AndAlso (Not listControls.Contains(listBox)) Then
-					listControls.Add(listBox)
-				End If
-				If TypeOf c Is SplitContainer Then
-					Dim splitContainer As SplitContainer = TryCast(c, SplitContainer)
-					FindListBox(splitContainer.Panel1, listControls)
-					FindListBox(splitContainer.Panel2, listControls)
-				End If
-				FindListBox(c, listControls)
-			Next c
-		End Sub
+        Private Sub pivotGridControl1_ShowingCustomizationForm(ByVal sender As Object, ByVal e As DevExpress.XtraPivotGrid.CustomizationFormShowingEventArgs)
+            _customizationForm = e.CustomizationForm
+        End Sub
 
+        Private Sub pivotGridControl1_ShowCustomizationForm(ByVal sender As Object, ByVal e As EventArgs)
+            If _customizationForm IsNot Nothing Then
+                Dim listControls As List(Of ListBoxControl) = New List(Of ListBoxControl)()
+                FindListBox(_customizationForm, listControls)
+                For Each listBox As ListBoxControl In listControls
+                    AddHandler listBox.MouseClick, New MouseEventHandler(AddressOf listBox_MouseClick)
+                Next
 
+                listControls.Clear()
+            End If
+        End Sub
 
+        Private Sub pivotGridControl1_HideCustomizationForm(ByVal sender As Object, ByVal e As EventArgs)
+            If _customizationForm IsNot Nothing Then
+                Dim listControls As List(Of ListBoxControl) = New List(Of ListBoxControl)()
+                FindListBox(_customizationForm, listControls)
+                For Each listBox As ListBoxControl In listControls
+                    RemoveHandler listBox.MouseClick, New MouseEventHandler(AddressOf listBox_MouseClick)
+                Next
 
+                listControls.Clear()
+                _customizationForm = Nothing
+            End If
+        End Sub
 
-	End Class
+        Private Sub listBox_MouseClick(ByVal sender As Object, ByVal e As MouseEventArgs)
+            Dim listBox As ListBoxControl = TryCast(sender, ListBoxControl)
+            Dim treeNode As PivotCustomizationTreeNodeBase = TryCast(listBox.SelectedItem, PivotCustomizationTreeNodeBase)
+            If treeNode IsNot Nothing Then Text = treeNode.Field.ToString()
+        End Sub
+
+        Private Sub FindListBox(ByVal parentControl As Control, ByVal listControls As List(Of ListBoxControl))
+            For Each c As Control In parentControl.Controls
+                Dim listBox As ListBoxControl = TryCast(c, ListBoxControl)
+                If listBox IsNot Nothing AndAlso Not listControls.Contains(listBox) Then listControls.Add(listBox)
+                If TypeOf c Is SplitContainer Then
+                    Dim splitContainer As SplitContainer = TryCast(c, SplitContainer)
+                    FindListBox(splitContainer.Panel1, listControls)
+                    FindListBox(splitContainer.Panel2, listControls)
+                End If
+
+                FindListBox(c, listControls)
+            Next
+        End Sub
+    End Class
 End Namespace
